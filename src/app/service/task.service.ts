ac4056939ebb170e438889ca6ee5817c08a1a09d
fanie <faniejohnsons98@gmail.com>
2023-11-27 15:55:35 +0200

    Local JSON Server Added

--- src/app/service/task.service.ts
+++ src/app/service/task.service.ts
@@ -1,17 +1,18 @@
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs'
-import { Task } from '../Task'
-import { TASKS } from '../mock-tasks'
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { Task } from '../Task';
+import { TASKS } from '../mock-tasks';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
+  private apiUrl = 'http://localhost:5000/tasks'
 
-  constructor(){}
+  constructor(private http:HttpClient){}
 
   getTasks(): Observable<Task[]> {
-    const tasks = of(TASKS)
-    return tasks;
+   return this.http.get<Task[]>(this.apiUrl)
   }
 }
